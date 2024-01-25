1e1fa8c35f296b45b93389445f9882cf0c43a1fc
marianapere <marianamirpereyra@gmail.com>
2024-01-24 21:14:04 -0300

    Resuelto M03 Java

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -66,6 +66,7 @@ function deEuroAdolar(euro) {
             return euro * 1.20
 }
 
+
 function esVocal(letra) {
    // Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”.
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
